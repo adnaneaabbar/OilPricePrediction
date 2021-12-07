469be6ba5274415bf633d4cdcb4b724452527a19
adnaneaabbar <adnaneaabbar@gmail.com>
2021-12-07 17:20:09 +0100

    plot and cleaning

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("data/DCOILBRENTEU.csv", sep=",")
 print(df.shape)
 
 print(df.head())
+
+#remove NA values
+df = df[df["DCOILBRENTEU"] != "."]
+print(df.shape)
+
+df_ind = df.set_index("DATE")
+
+# Visualising the Data
+df_ind = df_ind.astype(float)
+df_ind.plot()
+plt.show()
+plt.savefig("data/initial_data.png")
