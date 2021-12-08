e1fe7f62a7d15b8a3d1c10372ef61b241d9ad804
adnaneaabbar <adnaneaabbar@gmail.com>
2021-12-08 22:41:52 +0100

    train size

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from keras.preprocessing import sequence
+from keras.models import load_model
 
 df = pd.read_csv("data/DCOILBRENTEU.csv", sep=",")
 print(df.shape)
@@ -10,10 +13,28 @@ print(df.head())
 df = df[df["DCOILBRENTEU"] != "."]
 print(df.shape)
 
-df_ind = df.set_index("DATE")
+# df_ind = df.set_index("DATE")
+#
+# # Visualising the Data
+# df_ind = df_ind.astype(float)
+# df_ind.plot()
+# plt.savefig("data/data.png")
+# plt.show()
 
-# Visualising the Data
-df_ind = df_ind.astype(float)
-df_ind.plot()
-plt.savefig("data/data.png")
-plt.show()
+batch_size = 64
+epochs = 120
+timesteps = 10
+test_percent = 0.1
+
+# setting the training size
+def get_train_size(data, batch_size, test_percent):
+    n = len(data)
+    n *= (1 - test_percent)
+    if int(n) % batch_size == 0:
+        return int(n)
+    for i in range(int(n) - batch_size, int(n)):
+        if i % batch_size == 0:
+            return i
+
+print(len(df) * (1 - test_percent))
+print(get_train_size(df, batch_size, test_percent))
